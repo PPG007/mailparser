28ccfd71808e99d46ffbeb2f66e6d4d976b14aa6
Koston Zhuang <koston.zhuang@maiscrm.com>
2022-08-13 14:17:15 +0800

    finish backend

--- main.go
+++ main.go
@@ -22,10 +22,10 @@ var (
 	cookie string
 )
 
-func init() {
-	token = `QXltVG9rZW4tMTEzMjg4MTktY1o5cWtpV3N4c1l0YWYwVFVraXVBVG5uWkt0bzF5ZHpzb0F2UTNjMFlVWjNaOUdPclY`
-	cookie = `cna=DHdxG+Z9onwCAd0DBAu7d4LJ; aliyun_choice=CN; t=f01a3004efb070c6e066130d1cabdc10; login_aliyunid_pk=1191551862183498; aliyun_country=CN; aliyun_site=CN; currentRegionId=cn-hangzhou; isg=BHt7D1EWnW-ITKHc1GTIsCyFClbl0I_SoEHGsm04Q3qRzJqu9aOzIocF4myCa-fK; tfstk=c__CBNxXjIvw7b26QHNa4Ho-MGL5ZFu6wD9CORVSoj6qWIfCiiu2h4VJqA8ywC1..; l=eBa1PbOrL7BM36e5BOfZourza7797IRf_uPzaNbMiOCP_D5p5K7dW6xIpLT9CnGVHsKwR3yJlgPkB-YwWzO-nxv9-zq4RqHmndC..; alimail_sso_umid=wV12z62edf1586f5bb9db00076ec277b7; alimail_init_lang=zh_CN; alimail_browser_instance=dC0xMjQ0NzktU1ltbFBi8981; alimail_sid=I1666AA1-M1Z22HIMD1MV6A3WQI1R3-DI7YAR6L-AK91; alimail_sdata0=a24zos5gOAbHitWQr5w%2FAFIJUuxjvn9tro1VNQUp0SDTYxFPuqaJ9Ghd5NAatIf7agYva%2B6TI%2FC0T2sQfy7gr2QBQbpYUBlAzu73GSDSBU%2FnQRZJCk%2FaZvF2ItoVfuR3s865DBAPd9OngeDNbbAirg%3D%3D; core_token="UNIFY:5a97cab2-61a2-48fe-85c3-781bc2de06d0|47f255d8-121b-47d4-8f52-1e2e4bc120a9"; core_heart_beat=1660359348054; alimail_core_session_key=QXltU2Vzc2lvbi00NzI1OS1iYUlKSWpUYURFR0gyWFZRcjA0TVNxNmZIaVpVQjUxRnRNb1VhVG5oZFRsWFdEU3pZSw; alimail_auth_mode=core; alimail_auth_session_key=QXltU2Vzc2lvbi00NzI2MC1OaW5FcEQ3Y3FoSlNrU3VmTEw1YWJxQkhsSDNBUkxxTEgwaHIxS0tZS3FBZmNJT0QwdQ; alimail_session_version_key=0.1.35; _csrf_token_=QXltVG9rZW4tMTEzMjg4MTktY1o5cWtpV3N4c1l0YWYwVFVraXVBVG5uWkt0bzF5ZHpzb0F2UTNjMFlVWjNaOUdPclY; at="koston.zhuang@maiscrm.comXhKEk1660359349571"; alimail_session_template_key=v4; udtoken="koston.zhuang@maiscrm.com:9e2eb7db073bbd12be13c3cf079e8f06:6460581660359354603424"`
-	length = 5
+type Updater struct {
+	Length int    `json:"length" binding:"required"`
+	Token  string `json:"token" binding:"required"`
+	Cookie string `json:"cookie" binding:"required"`
 }
 
 type AliMailResponse struct {
@@ -38,9 +38,14 @@ type AliMailData struct {
 	Body      string `json:"encSummary"`
 }
 
-type ResultResponse struct {
+type Result struct {
 	SendAt  string `json:"sendAt"`
 	Content string `json:"content"`
+}
+
+type ResultResponse struct {
+	Items []Result `json:"items"`
+	Total int      `json:"total"`
 }
 
 func postOnce(offset int) (*AliMailResponse, error) {
@@ -83,43 +88,38 @@ func getDailyMailForSingleWeek() ([]AliMailData, error) {
 	return result, nil
 }
 
-func Init(ctx *gin.Context) {
-	type Updater struct {
-		Length int    `json:"length"`
-		Token  string `json:"token"`
-		Cookie string `json:"cookie"`
-	}
+func SearchMails(ctx *gin.Context) {
 	updater := Updater{}
 	err := ctx.ShouldBindBodyWith(&updater, binding.JSON)
 	if err != nil {
 		ctx.String(http.StatusInternalServerError, err.Error())
 		return
 	}
+
 	token = updater.Token
 	length = updater.Length
 	cookie = updater.Cookie
-	ctx.String(http.StatusOK, "ok")
-}
 
-func GetMails(ctx *gin.Context) {
 	datas, err := getDailyMailForSingleWeek()
 	if err != nil {
 		ctx.String(http.StatusInternalServerError, err.Error())
 		return
 	}
-	resps := []ResultResponse{}
+	results := []Result{}
 	for _, data := range datas {
-		resps = append(resps, ResultResponse{
+		results = append(results, Result{
 			SendAt:  TransMillTimestampToTime(data.TimeStamp).Format(TIME_FORMATTER),
 			Content: ParseAndConcatSingleDayMail(data.Body),
 		})
 	}
-	ctx.JSON(http.StatusOK, resps)
+	ctx.JSON(http.StatusOK, ResultResponse{
+		Total: len(results),
+		Items: results,
+	})
 }
 
 func main() {
 	e := gin.Default()
-	e.GET("/mails", GetMails)
-	e.POST("/init", Init)
+	e.POST("/mails/search", SearchMails)
 	e.Run(":8080")
 }
